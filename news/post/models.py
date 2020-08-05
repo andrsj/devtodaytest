4e6d5a8cf911cbdd7e082f49cc59b5a817df3d80
andrsj <andrsj.derkach@gmail.com>
2020-08-05 22:55:38 +0300

    Done simple django MVP

---
+++ news/post/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    count_upvotes = models.IntegerField(default=0, editable=False)
+    author_name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['creation_date', 'title']
+
+
+class Comment(models.Model):
+    author_name = models.CharField(max_length=255)
+    content = models.CharField(max_length=1024)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
---
+++ news/post/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from .models import Post, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    comments = serializers.SlugRelatedField(many=True, slug_field='content', read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'link', 'creation_date', 'count_upvotes', 'author_name', 'comments',)
+        read_only_fields = ('count_upvotes', )
